da41a7d02b6a6130ec050ab9c519654bbc860cc2
Delcor <matheusdelcorbm@gmail.com>
2023-09-29 15:44:42 -0300

    att01

--- java-training/CursoJava06.java
+++ java-training/CursoJava06.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class CursoJava06 {
     public static void main(String[] args) {
-        
+        final int size = 5;
+        char[] template = { 'a', 'a', 'd', 'b', 'c' };
+        char[] answers = new char[size];
+        int note = 0;
+        Scanner keyboard = new Scanner(System.in);
     }
 }
